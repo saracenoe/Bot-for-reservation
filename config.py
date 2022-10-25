606f12819e1c106d92d715a8b8c1e62fb4848664
saracenoe <receptorinteraction@gmail.com>
2022-10-25 02:30:48 +0200

    add new value from bot_last client

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class DefaultConfig:
     PORT = 8000
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
     APP_ID = "315267ba-6800-4298-bb32-5dd8a7948435" #os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = "RpC8Q~OaTmz1YLQ5MnX0toIdSvcJrYA4nnI5Cc0." #os.environ.get("MicrosoftAppPassword", "")
+    APP_PASSWORD = "eaab3185-3119-45e7-9e9b-951b0dd92677" #os.environ.get("MicrosoftAppPassword", "")
     LUIS_APP_ID = os.environ.get("LuisAppId", "")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
