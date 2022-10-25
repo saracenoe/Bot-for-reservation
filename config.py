76752bc39f5428b1f0e462f6b338231cdf9373d6
saracenoe <116607806+saracenoe@users.noreply.github.com>
2022-10-25 13:17:22 +0200

    Delete visible keys

--- config.py
+++ config.py
@@ -18,9 +18,8 @@ class DefaultConfig:
     # PORT = 3978 # en local
     PORT = 8000
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
-    APP_ID = "315267ba-6800-4298-bb32-5dd8a7948435" #os.environ.get("MicrosoftAppId", "")
-    # APP_PASSWORD = "eaab3185-3119-45e7-9e9b-951b0dd92677" #os.environ.get("MicrosoftAppPassword", "")
-    APP_PASSWORD = "D5i8Q~TZ5~GkQ8pPeJI3tsR9tZYZ.8-qFmZYWahG" #os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
     LUIS_APP_ID = os.environ.get("LuisAppId", "")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
