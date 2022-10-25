0a940293abccf890d4975c984de78268747fd7e2
ezesaraceno <ezequielsaraceno@gmail.com>
2022-10-25 02:09:05 +0200

    first push of bot folder

---
+++ config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""Configuration for the bot."""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class DefaultConfig:
+    """Configuration for the bot."""
+
+
+
+    # PORT = 3978 # en local
+    PORT = 8000
+    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
+    APP_ID = "315267ba-6800-4298-bb32-5dd8a7948435" #os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = "RpC8Q~OaTmz1YLQ5MnX0toIdSvcJrYA4nnI5Cc0." #os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
+        "AppInsightsInstrumentationKey", ""
+    )
---
+++ dialogs/cancel_and_help_dialog.py
@@ -0,0 +1,67 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""Handle cancel and help intents."""
+
+from botbuilder.core import BotTelemetryClient, NullTelemetryClient
+from botbuilder.dialogs import (
+    ComponentDialog,
+    DialogContext,
+    DialogTurnResult,
+    DialogTurnStatus,
+)
+from botbuilder.schema import ActivityTypes
+
+
+class CancelAndHelpDialog(ComponentDialog):
+    """Implementation of handling cancel and help."""
+
+    def __init__(
+        self,
+        dialog_id: str,
+        telemetry_client: BotTelemetryClient = NullTelemetryClient(),
+    ):
+        """Initialize a new CancelAndHelpDialog instance."""
+        super().__init__(dialog_id)
+        self.telemetry_client = telemetry_client
+
+    async def on_begin_dialog(
+        self, inner_dc: DialogContext, options: object
+    ) -> DialogTurnResult:
+        """Handle the begin dialog event."""
+        result = await self.interrupt(inner_dc)
+        if result is not None:
+            return result
+
+        return await super().on_begin_dialog(inner_dc, options)
+
+    async def on_continue_dialog(self, inner_dc: DialogContext) -> DialogTurnResult:
+        """Handle the continue dialog event."""
+        result = await self.interrupt(inner_dc)
+        if result is not None:
+            return result
+
+        return await super().on_continue_dialog(inner_dc)
+
+    async def interrupt(self, inner_dc: DialogContext) -> DialogTurnResult:
+        """Detect interruptions."""
+        if inner_dc.context.activity.type == ActivityTypes.message:
+            text = inner_dc.context.activity.text.lower()
+
+            if "help" in text or "?" in text:
+                await inner_dc.context.send_activity(
+                    """
+🏙️ Just tell me **where** you want to travel to (cities of origin and destination).
+Ex.:'I want to travel from Seattle to San Francisco'\n
+📅 I will also need to know **when** you want to travel (dates of departure and return).
+Ex.:'I want to travel on May 1, 2020 and return on May 5, 2020'\n
+💸 Finally, you can give me a **budget** for your trip.
+Ex.:'I want to travel for $500'\n
+We can start over from scratch anytime if you just say _'Cancel'_"""
+                )
+                return DialogTurnResult(DialogTurnStatus.Waiting)
+
+            if text in ("cancel", "quit"):
+                await inner_dc.context.send_activity("It's OK to change your mind")
+                return await inner_dc.cancel_all_dialogs()
+
+        return None
