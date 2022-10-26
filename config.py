59a45679f0e07723516a2395049fd14ed7ed1cb9
saracenoe <116607806+saracenoe@users.noreply.github.com>
2022-10-26 19:22:18 +0200

    Add cles

--- config.py
+++ config.py
@@ -18,11 +18,11 @@ class DefaultConfig:
     # PORT = 3978 # en local
     PORT = 8000
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    APP_ID = "315267ba-6800-4298-bb32-5dd8a7948435" #os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = "D5i8Q~TZ5~GkQ8pPeJI3tsR9tZYZ.8-qFmZYWahG" #os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = "099431fb-d90d-44b6-a5cf-82a9d8f804e7" #os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = "93d125472bfd46c5ae0e599fcb8207c8"#os.environ.get("LuisAPIKey", "")
+    LUIS_API_HOST_NAME = "westus.api.cognitive.microsoft.com"#os.environ.get("LuisAPIHostName", "")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey", ""
     )
