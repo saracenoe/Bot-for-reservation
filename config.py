dae9257016376d118fe2b16b34595fcc146af439
saracenoe <receptorinteraction@gmail.com>
2022-10-26 21:03:44 +0200

    changed in config(keys) and added aiohttp in requirements

--- config.py
+++ config.py
@@ -18,11 +18,11 @@ class DefaultConfig:
     # PORT = 3978 # en local
     PORT = 8000
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
-    APP_ID = "315267ba-6800-4298-bb32-5dd8a7948435" #os.environ.get("MicrosoftAppId", "")
-    # APP_PASSWORD = "eaab3185-3119-45e7-9e9b-951b0dd92677" #os.environ.get("MicrosoftAppPassword", "")
-    APP_PASSWORD = "D5i8Q~TZ5~GkQ8pPeJI3tsR9tZYZ.8-qFmZYWahG" #os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "315267ba-6800-4298-bb32-5dd8a7948435")
+    APP_PASSWORD =  os.environ.get("MicrosoftAppPassword", "eaab3185-3119-45e7-9e9b-951b0dd92677")
+    #APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "D5i8Q~TZ5~GkQ8pPeJI3tsR9tZYZ.8-qFmZYWahG")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "099431fb-d90d-44b6-a5cf-82a9d8f804e7")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "93d125472bfd46c5ae0e599fcb8207c8")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         "AppInsightsInstrumentationKey", ""
