d1f17ffae358cf4f73d2678ffb649ea4105bd100
saracenoe <116607806+saracenoe@users.noreply.github.com>
2022-10-26 20:05:25 +0200

    CAmbie APP pasword por secret ID

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class DefaultConfig:
     PORT = 8000
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
     APP_ID = "315267ba-6800-4298-bb32-5dd8a7948435" #os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = "D5i8Q~TZ5~GkQ8pPeJI3tsR9tZYZ.8-qFmZYWahG" #os.environ.get("MicrosoftAppPassword", "")
+    APP_PASSWORD = "eaab3185-3119-45e7-9e9b-951b0dd92677" #os.environ.get("MicrosoftAppPassword", "")
     LUIS_APP_ID = "099431fb-d90d-44b6-a5cf-82a9d8f804e7" #os.environ.get("LuisAppId", "")
     LUIS_API_KEY = "93d125472bfd46c5ae0e599fcb8207c8"#os.environ.get("LuisAPIKey", "")
     LUIS_API_HOST_NAME = "westus.api.cognitive.microsoft.com"#os.environ.get("LuisAPIHostName", "")
