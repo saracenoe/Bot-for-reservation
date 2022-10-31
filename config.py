2ca459b00c5b8644d5f8c277cedc5f3da6ec63a2
saracenoe <116607806+saracenoe@users.noreply.github.com>
2022-10-31 20:37:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class DefaultConfig:
     PORT = 8000
     APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
     APP_ID = os.environ.get("MicrosoftAppId", "315267ba-6800-4298-bb32-5dd8a7948435")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "eaab3185-3119-45e7-9e9b-951b0dd92677")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "D5i8Q~TZ5~GkQ8pPeJI3tsR9tZYZ.8-qFmZYWahG")
     LUIS_APP_ID = os.environ.get("LuisAppId", "099431fb-d90d-44b6-a5cf-82a9d8f804e7")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "93d125472bfd46c5ae0e599fcb8207c8")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westus.api.cognitive.microsoft.com")
